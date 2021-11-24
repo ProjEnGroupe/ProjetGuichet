f5a00405cd1786e3edd3b60d698f114f7643ebdf
abdelerrouissi <91915643+abdelerrouissi@users.noreply.github.com>
2021-11-24 13:11:28 -0500

    Add files via upload

---
+++ Guichet.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guichet
+{
+    public class Guichet
+    {
+       public  void afficherMenu()
+        {
+            Console.WriteLine("aaaa");
+            Console.WriteLine("---------------Menu guichet automatique-----------");
+            Console.WriteLine();
+            Console.WriteLine("--------Veuillez choisir l'une des actions suivantes------");
+            Console.WriteLine();
+            Console.WriteLine("1-Se connecter à votre compte");
+            Console.WriteLine("2-Se connecter comme administrateur. ");
+            Console.WriteLine("3-Quitter. ");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("-----------Faites votres choix svp-----------");
+            string choix = Console.ReadLine();
+            switch (choix)
+            {
+                case "1":
+                    AfficherMenuUtilisateur();
+                    break;
+                case "2":
+                    AfficherMenuAdministrateur();
+                    break;
+                case "3":
+                    Environment.Exit(0);
+                    break;
+                    afficherMessageErreur();
+                    break;
+            }
+        }
+        public  void afficherMessageErreur()
+        {
+            Console.WriteLine("LE CHOIX N'EST PAS VALIDE ");
+        }
+        public void afficherMenuUtilisateur() 
+        {
+            Console.WriteLine();
+        }
+
+    }
+}
