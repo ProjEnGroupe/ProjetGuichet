43a74a4e2d847e12004f6a568127080afaf0377a
ProjEnGroupe <94855318+ProjEnGroupe@users.noreply.github.com>
2021-11-25 13:22:05 -0500

    realiser la methode: afficherListClient()

--- ProjetGuichet-main/Guichet/Administrateur.cs
+++ ProjetGuichet-main/Guichet/Administrateur.cs
@@ -12,7 +12,33 @@ namespace Guichet
         /// </summary>
         public string ResetState() => Etat.ACTIVE.ToString();
 
+        public void RemettreGuichet()
+        {
+            if (ResetState().Equals("DEACTIVE"))
+            {
+                Console.WriteLine("Entrer votre nom");
+                string nom = Console.ReadLine();
+                Console.WriteLine("Entrer votre nip");
+                string nip = Console.ReadLine();
 
+            }
+        }
+
+        public void DeposerArgent() { }
+
+        public void VoirSolde() { }
+
+        public void afficherListClient()
+        {
+            foreach(Client c in Guichet.clients)
+            {
+                Console.WriteLine(value: $"\tNom: {c.Nom}, CompteEpargne: {c.CompteEpargne}, CompteCheck: {c.CompteCheck}, Etat: {c.Etat}");
+            }
+
+
+        }
+
+        public void MainMenu() { }
 
 
     }
