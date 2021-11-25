ff8f52a48adbeb1fcc0949c10d0f68c0a507e74e
ProjEnGroupe <94855318+ProjEnGroupe@users.noreply.github.com>
2021-11-25 08:31:30 -0500

    State: Acitve; Deactive, montant=10000,

---
+++ Guichet/Guichet/CompteClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guichet
+{
+    abstract class CompteClient
+    {
+        protected string numerocompte;
+        
+        public CompteClient()
+        {
+            
+        }
+
+        
+    }
+}
