8ee69234cf702dfb07355f266c503dcfa551d3a5
abdelerrouissi <91915643+abdelerrouissi@users.noreply.github.com>
2021-11-30 14:30:10 -0500

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+   public abstract class Client
+    {
+        public string Nom { get; set; }
+        public string Password { get; set; }
+       
+        public Client(string nom, string password)
+        {
+            Nom = nom;
+            Password = password;
+        }
+        public abstract void DeposMontant(double montant);
+       public abstract void RetirerMontant(double montant);
+       public abstract void AfficherSolde();
+       public abstract void PayerFacture(string numFacture, double montant);
+     }
+}
---
+++ CompteCheque.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    public class CompteCheque : Client
+    {
+        public CompteCheque(string nom, string password, string numeroCompte, double solde) : base(nom, password)
+        {
+            this.NumeroCompte = numeroCompte;
+            this.Solde = solde;
+        }
+
+        public string NumeroCompte { get; set; }
+        public double Solde { get; set; }
+        public override void AfficherSolde()
+        {
+            Console.WriteLine("Solde compte chéque :  {0}", this.Solde);
+        }
+
+        public override void DeposMontant(double montant)
+        {
+            this.Solde += montant;
+        }
+
+        public override void PayerFacture(string Facture, double montant)
+        {
+            RetirerMontant(montant + 2);
+            Console.WriteLine("Réglement du facture : {0}", Facture);
+        }
+      
+        public override void RetirerMontant(double montant)
+        {
+           this.Solde -= montant;
+        }
+
+        public void Virement(CompteEpargne epargne, double montant)
+        {
+            epargne.Solde += montant;
+            this.Solde -= montant;
+        }
+    }
+}
